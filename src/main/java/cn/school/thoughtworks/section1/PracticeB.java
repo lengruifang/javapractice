ff046017f1833b919f37aee818ff7f89b34a4f77
陈俊生 <2810955497@qq.com>
2021-09-25 17:11:16 +0800

    一次修改，优化了从第二个参数中选取字符串的方法

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,11 +1,18 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return collection1.stream().filter(elem->collection2.get(0).contains(elem)).collect(Collectors.toList());
+        List<String> strings=new ArrayList<>();
+        for (List<String> s1 : collection2) {
+            for (String s2 : s1) {
+                strings.add(s2);
+            }
+        }
+        return collection1.stream().filter(elem->strings.contains(elem)).collect(Collectors.toList());
     }
 }

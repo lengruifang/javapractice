1e75213bf7a5a0912a0cf12f0821287a2855d57b
陈俊生 <2810955497@qq.com>
2021-09-26 16:18:42 +0800

    修改了没有意义的变量名

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -8,9 +8,9 @@ public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
         List<String> strings=new ArrayList<>();
-        for (List<String> s1 : collection2) {
-            for (String s2 : s1) {
-                strings.add(s2);
+        for (List<String> childCollection : collection2) {
+            for (String string : childCollection) {
+                strings.add(string);
             }
         }
         return collection1.stream().filter(elem->strings.contains(elem)).collect(Collectors.toList());
